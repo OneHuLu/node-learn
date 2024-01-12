6a86b8dcf72b2cb73c7b5af1fc79dada389b080e
pang <1973503139@qq.com>
2024-01-12 21:42:11 +0800

    --feat Secure handling

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const expressRateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandle = require('./controller/errorController');
@@ -9,6 +13,9 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
+
+// Set security Http Header
+app.use(helmet());
 
 // 1) 中间件 middleware
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
@@ -21,8 +28,33 @@ const limiter = expressRateLimit({
 });
 app.use('/api', limiter);
 
+// Body parser, reading data from body into req.body
 app.use(express.json());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against xss
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+
+// Server static files
 app.use(express.static(`${__dirname}/public`));
+
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toDateString();
   next();
