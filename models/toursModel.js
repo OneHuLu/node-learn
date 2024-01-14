38fb38d260501ba640b9d850b9e8381803378009
pang <1973503139@qq.com>
2024-01-14 19:42:38 +0800

    --feat virtual populate

--- models/toursModel.js
+++ models/toursModel.js
@@ -120,6 +120,13 @@ tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
 
+// Virtual populate
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id'
+});
+
 // DOCUMENT MIDDLEWARE: runs before  .save() and .create() | (运行在保存和创建前，但是对与更新无效)
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
