2e245f58a12488927f9903e2eb8949f472348104
pang <1973503139@qq.com>
2024-01-14 20:45:50 +0800

    --feat Nested Routes Review On Tour

--- controller/reviewController.js
+++ controller/reviewController.js
@@ -3,15 +3,21 @@ const Review = require('../models/reviewModel');
 const { CatchAsyncError } = require('../utils/catchAsync');
 
 exports.getAllReview = CatchAsyncError(async (req, res, next) => {
-  const reviews = await Review.find();
+  const filter = {};
+  if (req.params.tourId) filter.tour = req.params.tourId;
+
+  const reviews = await Review.find(filter);
 
   res.status(200).json({
     status: 'success',
+    length: reviews.length,
     data: reviews
   });
 });
 
 exports.createReview = CatchAsyncError(async (req, res, next) => {
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
   const newTReview = await Review.create(req.body);
   res.status(200).json({
     status: 'success',
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,8 +2,12 @@ const express = require('express');
 // controller for tours
 const tourController = require('../controller/tourController');
 const authController = require('../controller/authController');
+const reviewRouter = require('../routes/reviewRoutes');
 
 const router = express.Router();
+
+// Nested Routes
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/')
