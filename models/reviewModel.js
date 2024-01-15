9c4617916f9396b402d9ed4238e761861f879ab6
pang <1973503139@qq.com>
2024-01-15 23:09:20 +0800

    --feat Calcuating Average Rating on Tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./toursModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -47,6 +48,52 @@ reviewSchema.pre(/^find/, function(next) {
   });
 
   next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function(tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: {
+        tour: tourId
+      }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5
+    });
+  }
+};
+
+reviewSchema.post('save', function() {
+  // this points to current document
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+// DU1)这里拿到未改变前的文档数据,获取tour的id根据这个id计算平均值
+reviewSchema.pre(/^findOneAnd/, async function(next) {
+  this.r = await this.findOne();
+  next();
+});
+
+// DU2) 这个时候的数据已经改变了，计算品均值
+reviewSchema.post(/^findOneAnd/, async function() {
+  // await this.findOne(); 这个方法在这里执行无效，因为他已经执行过了
+  await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
