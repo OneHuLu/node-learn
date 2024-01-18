82337cdfee2af1c02e4bd0a163fb78ea13102755
pang <1973503139@qq.com>
2024-01-18 22:44:24 +0800

    --fix Spelling mistakes

--- models/toursModel.js
+++ models/toursModel.js
@@ -87,7 +87,7 @@ const tourSchema = new mongoose.Schema(
       address: String,
       description: String
     },
-    location: [
+    locations: [
       {
         type: {
           type: String,
