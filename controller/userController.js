103cbc3f8d2e7c34dab4244a4a83ee21b543adba
pang <1973503139@qq.com>
2024-02-01 15:27:25 +0800

    --refactor filter allowed fields

--- controller/userController.js
+++ controller/userController.js
@@ -1,12 +1,11 @@
 const User = require('../models//userModel');
-const AppError = require('../utils/appError');
 const handleFactory = require('./handleFactory');
 const { CatchAsyncError } = require('../utils/catchAsync');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
   Object.keys(obj).map(item => {
-    if (allowedFields.includes(item)) newObj[item] = obj[item];
+    if (!allowedFields.includes(item)) newObj[item] = obj[item];
     return null;
   });
   return newObj;
@@ -17,27 +16,16 @@ exports.getMe = (req, res, next) => {
 };
 
 exports.updateMe = CatchAsyncError(async (req, res, next) => {
-  // 1) Create error if user post password data. TODO: I think only do a filter here
-  if (req.body.passwordConfirm || req.body.password) {
-    return next(
-      new AppError(
-        'This route is not for change password.Please use /updatePassword',
-        400
-      )
-    );
-  }
-  // 2) Filtered out you unwanted  fields names that are not allowed be updated.
-  const filterBody = filterObj(req.body, 'name', 'email');
-  // 3) Update user document
+  // 1) Filtered out you unwanted  fields names that are not allowed be updated.
+  const filterBody = filterObj(req.body, 'passwordConfirm', 'password', 'role');
+  // 2) Update user document
   const updateUser = await User.findByIdAndUpdate(req.user.id, filterBody, {
     new: true,
     runValidators: true
   });
   res.status(200).send({
     status: 'success',
-    data: {
-      user: updateUser
-    }
+    data: updateUser
   });
 });
 
