fd4461cac3733f4018e329807b9e0f2c9986735e
pang <1973503139@qq.com>
2024-03-03 22:30:26 +0800

    --refactor 重构请求流程

--- controller/gptsController.js
+++ controller/gptsController.js
@@ -1,18 +1,27 @@
 const OpenAIApi = require('openai');
 
-// 这个process.env.OPENAI_API_KEY本地可以获取，线上就获取不到。。
-const config = { apiKey: process.env.OPENAI_API_KEY };
+//TODO: 这个process.env.OPENAI_API_KEY本地可以获取，线上就获取不到。。
 
-// 开发启用本地代理 
-if (process.env.NODE_ENV === 'development') {
-  const { HttpsProxyAgent } = require('https-proxy-agent');
-  const httpAgent = new HttpsProxyAgent('http://127.0.0.1:7890');
-  config.httpAgent = httpAgent;
-}
+// 开发环境配置
+exports.setConfig = (req, res, next) => {
+  // 配置key
+  const config = { apiKey: process.env.OPENAI_API_KEY };
+  // 开发启用本地代理
+  if (process.env.NODE_ENV === 'development') {
+    const { HttpsProxyAgent } = require('https-proxy-agent');
+    const httpAgent = new HttpsProxyAgent('http://127.0.0.1:7890');
+    config.httpAgent = httpAgent;
+  }
 
-const openai = new OpenAIApi(config);
+  const openai = new OpenAIApi(config);
+  // 将openai方法传递下去，存储在req对象中
+  req.openai = openai;
+  next();
+};
 
+// 询问方法
 exports.ask = async (req, res, next) => {
+  const { openai } = req;
   const { content } = req.body;
   const completion = await openai.chat.completions.create({
     messages: [{ role: 'system', content }],
@@ -20,7 +29,6 @@ exports.ask = async (req, res, next) => {
   });
 
   res.status(200).json({
-    status: 200,
     data: completion.choices[0],
   });
 };
--- routes/gptsRoutes.js
+++ routes/gptsRoutes.js
@@ -4,6 +4,7 @@ const gptsController = require('../controller/gptsController.js');
 
 const router = express.Router();
 
+router.use(gptsController.setConfig)
 router.route('/ask').post(gptsController.ask);
 
 module.exports = router;
